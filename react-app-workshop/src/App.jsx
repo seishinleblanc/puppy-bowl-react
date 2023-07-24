be9960b89df080f69e47346672d4ec93bf5763be
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-07-24 13:15:34 -0500

    Completed first stage of workshop.

--- react-app-workshop/src/App.jsx
+++ react-app-workshop/src/App.jsx
@@ -1,12 +1,18 @@
-
 import './App.css'
+import {useState} from 'react'
+import {puppyList} from "./data.js"
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
     <>
-      
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
